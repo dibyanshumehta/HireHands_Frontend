cc5653fd2a65e2284ca879fbc995cd11ca38a671
dibyanshumehta <mehtadibyanshu2004@gmail.com>
2026-06-20 12:53:22 +0530

    cloud backend api added

--- src/data-access/api/user/user-auth-api.js
+++ src/data-access/api/user/user-auth-api.js
@@ -5,7 +5,7 @@ export const postUserRegistrationData = createAsyncThunk(
     "userAuth/postUserRegistrationData",
     async (FormUserData, thunkAPI) => {
         try{
-            let userdata = await axios.post("http://localhost:9000/auth/usersignup", FormUserData);
+            let userdata = await axios.post("https://hirehands-backend.onrender.com/auth/usersignup", FormUserData);
             console.log(userdata);
             return thunkAPI. fulfillWithValue(userdata);
         } catch (error) {
@@ -18,7 +18,7 @@ export const postUserLoginData = createAsyncThunk(
     "userAuth/postUserLoginData",
     async (FormUserData, thunkAPI) => {
         try {
-            let  data  = await axios.post("http://localhost:9000/auth/usersignin", FormUserData);
+            let  data  = await axios.post("https://hirehands-backend.onrender.com/auth/usersignin", FormUserData);
            
             if (data.status === 201){
                 localStorage.setItem( "authToken", data.data.token);
